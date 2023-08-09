8da5985487ab1d797b7addf8c0b9734d89d70775
Vaginderpal Singh Brar <137271458+Vaginderpal@users.noreply.github.com>
2023-08-10 00:09:25 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
+task("accounts", "Prints the list of all accounts", async (taskArgs,hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for(const account of accounts){
+    console.log(account);
+  }
+})
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+    },
     mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
+      url: process.env.MUMBAI_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
     goerli: {
-      url: 'https://ethereum-goerli.publicnode.com',
+      url: "https://rpc.ankr.com/eth_goerli",
       accounts: [process.env.PRIVATE_KEY],
     },
-  }
+  },
 };
