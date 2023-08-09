4b9b8875db24540a29558a4e895cf967401e10b1
Vaginderpal Singh Brar <137271458+Vaginderpal@users.noreply.github.com>
2023-08-10 00:06:18 +0530

    getBalance.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,30 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-require('dotenv').config()
-
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const nftAddress = "0xE6db7ebB38Fb54adA74b44f4C55601CB4623DE66"; // contract address
 
 async function main() {
 
-    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-  
-    const tx = await token.mint(walletAddress, 1000);
-    await tx.wait();
+    const [signer] = await ethers.getSigners();
+    const NFTContract = await ethers.getContractFactory("BrarNFTs");
+    const nftcontract = await NFTContract.attach(nftAddress);
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+    console.log("You now have: " + await nftcontract.balanceOf(signer.address) + " nfts");
+}
+
+main().catch((error) => {
+console.error(error);
+process.exitCode = 1;
+});
